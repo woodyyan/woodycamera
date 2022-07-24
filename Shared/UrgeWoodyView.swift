9a2a5bb387b9d3d3fc7aa7c6d1bf64caf9a265fe
Woody <colorguitar@hotmail.com>
2022-07-24 18:29:18 +0800

    finish urge view

--- Shared/UrgeWoodyView.swift
+++ Shared/UrgeWoodyView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 struct UrgeWoodyView: View {
-    @State private var showingAlert = false
+    @State private var showingSuccessAlert = false
+    @State private var showingFailAlert = false
     let urgeCount = 1
 
     @ViewBuilder
@@ -15,7 +16,12 @@ struct UrgeWoodyView: View {
             }
             
             Button {
-                showingAlert = true
+                let success = urge()
+                if success {
+                    showingSuccessAlert = true
+                } else {
+                    showingFailAlert = true
+                }
             } label: {
                 Label("Woody快更新！", systemImage: "camera")
             }
@@ -24,13 +30,73 @@ struct UrgeWoodyView: View {
                 RoundedRectangle(cornerRadius: 30)
                     .stroke(Color.themeColor, lineWidth: 1)
             )
-            .alert("催更成功", isPresented: $showingAlert) {
+            .alert("催更成功", isPresented: $showingSuccessAlert) {
                 Button("好的", role: .cancel) { }
             } message: {
                 Text("Woody已经收到你的催更")
             }
+            .alert("催更失败", isPresented: $showingFailAlert) {
+                Button("好的", role: .cancel) { }
+            } message: {
+                Text("你今天已经催过了哦，明天再来催吧。")
+            }
             Label("点它催Woody更新", systemImage: "hand.point.up")
                 .font(.system(size: 14)).padding()
+        }
+    }
+    
+    func urge() -> Bool {
+        let today = Date.now
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let urgeDate = formatter.string(from: today)
+        let file = "urge.json"
+
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+
+            let fileURL = dir.appendingPathComponent(file)
+            
+            //reading
+            do {
+                let date = try String(contentsOf: fileURL, encoding: .utf8)
+                print(date)
+                if let savedDate = formatter.date(from:date) {
+                    let success = !isSameDay(date1: savedDate, date2: today)
+                    return success
+                }
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+
+            //writing
+            do {
+                try urgeDate.write(to: fileURL, atomically: false, encoding: .utf8)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        updateUrgeCount()
+        return true
+    }
+    
+    func updateUrgeCount() {
+        
+    }
+    
+    func getUrgeCount() -> Int {
+    
+        return 0
+    }
+    
+    func isSameDay(date1: Date, date2: Date) -> Bool {
+        let diff = Calendar.current.dateComponents([.day], from: date1, to: date2)
+        if diff.day == 0 {
+            return true
+        } else {
+            return false
         }
     }
 }
