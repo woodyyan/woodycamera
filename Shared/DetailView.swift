5c0935d9d87341877d8e60baacdfc00c7b4c84bc
Woody <colorguitar@hotmail.com>
2022-08-08 22:00:50 +0800

    add image cache

--- Shared/DetailView.swift
+++ Shared/DetailView.swift
@@ -1,18 +1,25 @@
 import SwiftUI
-import CachedAsyncImage
+import LBJImagePreviewer
 
 struct DetailView: View {
     let modelItem: ModelItem
     @State var selection = 0
+    @State private var zoomed: Bool = false
     @State private var isSharePresented: Bool = false
 
     var body: some View {
         TabView(selection: $selection) {
             ForEach(modelItem.images) { item in
                 ZStack {
-                    CachedAsyncImage(url: item.url) { image in
+                    AsyncImage(url: getLocalImageUrl(url: item.url)) { image in
                         image.resizable()
                             .scaledToFit()
+                            .aspectRatio(contentMode: self.zoomed ? .fill : .fit)
+                            .onTapGesture { // 注册点击事件
+                                withAnimation{ // 添加动画效果
+                                    self.zoomed.toggle() // toggle方法可以取Bool值得反值
+                                }
+                            }
                     } placeholder: {
                         ProgressView()
                     }
@@ -23,7 +30,6 @@ struct DetailView: View {
         .toolbar {
             ToolbarItem {
                 Button {
-                    print(self.selection)
                     self.isSharePresented = true
                 } label: {
                     Label("Share", systemImage: "square.and.arrow.up")
@@ -32,12 +38,63 @@ struct DetailView: View {
                 .sheet(isPresented: $isSharePresented, onDismiss: {
                     print("Dismiss")
                 }, content: {
-                    ActivityViewController(activityItems: [UIImage(data:try! Data(contentsOf: self.modelItem.images[self.selection].url)) as Any])
+                    ActivityViewController(activityItems: [UIImage(data:try! Data(contentsOf: self.getLocalImageUrl(url: self.modelItem.images[self.selection].url))) as Any])
                 })
             }
         }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func saveImage(image: UIImage, filename: String) -> Bool {
+        guard let data = image.jpegData(compressionQuality: 1) ?? image.pngData() else {
+            return false
+        }
+        let directory = getDocumentsDirectory()
+        do {
+            try data.write(to: directory.appendingPathComponent(filename))
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    func getLocalImageUrl(url: URL) -> URL {
+        let path = getDocumentsDirectory().appendingPathComponent(url.lastPathComponent)
+        
+        if FileManager.default.fileExists(atPath: path.path) {
+            return path
+        }
+        
+        self.getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            // always update the UI from the main thread
+            if saveImage(image: UIImage(data: data)!, filename: url.lastPathComponent) {
+                print("Save success")
+            }
+        }
+        return url
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        let dataPath = documentsDirectory.appendingPathComponent("images")
+        if !FileManager.default.fileExists(atPath: dataPath.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: dataPath.path, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return dataPath
     }
 }
 
